7639bf24bae6355cf4f199c55ff4f70ac0934588
Gabe Fierro <gtfierro225@gmail.com>
2013-12-02 11:54:24 -0800

    go fmt maingo

--- main.go
+++ main.go
@@ -10,12 +10,12 @@ import (
 
 func main() {
 	flag.Parse()
-    f, err := os.Create("cprof")
-    if err != nil {
-        log.Fatal(err)
-    }
-    pprof.StartCPUProfile(f)
-    defer pprof.StopCPUProfile()
+	f, err := os.Create("cprof")
+	if err != nil {
+		log.Fatal(err)
+	}
+	pprof.StartCPUProfile(f)
+	defer pprof.StopCPUProfile()
 	runtime.GOMAXPROCS(runtime.NumCPU())
 	filename := flag.Arg(0)
 	external := flag.Arg(1)
@@ -25,10 +25,10 @@ func main() {
 	} else {
 		calculateDistances()
 	}
-    f, err = os.Create("mprof")
-    if err != nil {
-        log.Fatal(err)
-    }
-    pprof.WriteHeapProfile(f)
-    f.Close()
+	f, err = os.Create("mprof")
+	if err != nil {
+		log.Fatal(err)
+	}
+	pprof.WriteHeapProfile(f)
+	f.Close()
 }
